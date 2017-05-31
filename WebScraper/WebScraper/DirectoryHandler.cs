3ae6553386268323d1d147fc65f7cb2a0b542025
Smurfa <pontus.franzen@live.se>
2017-05-31 21:42:29 +0200

    Add folder creation from URL.

---
+++ WebScraper/WebScraper/DirectoryHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WebScraper
+{
+    public class DirectoryHandler
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        public void CreateDirectory(string url)
+        {
+            var folderName = ExtractFoldername(url);
+            Directory.CreateDirectory(folderName);
+        }
+
+        private string ExtractFoldername(string url)
+        {
+            return url.Split(new[] { '/' }).Last();
+        }
+    }
+}
