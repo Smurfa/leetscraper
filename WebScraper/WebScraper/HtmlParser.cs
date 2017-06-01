35a25884868592f20816e4f45969b9eebf0572e2
Smurfa <pontus.franzen@live.se>
2017-06-01 21:12:54 +0200

    Add HtmlParser to extract href tags.

---
+++ WebScraper/WebScraper/HtmlParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace WebScraper
+{
+    public class HtmlParser
+    {
+        public IEnumerable<string> ExtractAHrefTags(string html)
+        {
+            var document = new HtmlDocument();
+            document.LoadHtml(html);
+
+            var nodes = document.DocumentNode.SelectNodes("//a[@href]");
+            if (nodes != null)
+            {
+                foreach (var node in nodes)
+                {
+                    yield return node.GetAttributeValue("href", null);
+                }
+            }
+        }
+    }
+}
