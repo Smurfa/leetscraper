ab10d40b3a0a9633e7a1589fc173b63235c3bd31
Smurfa <pontus.franzen@live.se>
2017-06-03 13:59:42 +0200

    Update HtmlParserl with ExtractLinkHrefTags and ExtractScriptSrcTags.

--- WebScraper/WebScraper/HtmlParser.cs
+++ WebScraper/WebScraper/HtmlParser.cs
@@ -23,5 +23,35 @@ namespace WebScraper
                 }
             }
         }
+
+        public IEnumerable<string> ExtractLinkHrefTags(string html)
+        {
+            var document = new HtmlDocument();
+            document.LoadHtml(html);
+
+            var nodes = document.DocumentNode.SelectNodes("//link[@href]");
+            if (nodes != null)
+            {
+                foreach (var node in nodes)
+                {
+                    yield return node.GetAttributeValue("href", null);
+                }
+            }
+        }
+
+        public IEnumerable<string> ExtractScriptSrcTags(string html)
+        {
+            var document = new HtmlDocument();
+            document.LoadHtml(html);
+
+            var nodes = document.DocumentNode.SelectNodes("//script[@src]");
+            if (nodes != null)
+            {
+                foreach (var node in nodes)
+                {
+                    yield return node.GetAttributeValue("src", null);
+                }
+            }
+        }
     }
 }
