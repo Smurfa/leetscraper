5114bc02d39a3ceb72baefe8bf7e18ee6b80bb9c
Smurfa <pontus.franzen@live.se>
2017-06-06 18:54:19 +0200

    Add test cases for Scraper if URL is missing http. Add test of getting content URLs.

--- WebScraper/TestCases/TestScraper.cs
+++ WebScraper/TestCases/TestScraper.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Text;
-using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WebScraper;
@@ -29,10 +29,24 @@ namespace TestCases
         /// Tries to download a file and verify if the file was downloaded.
         /// </summary>
         [TestMethod]
-        public async Task DownLoadFile_UrlOk()
+        public async Task DownLoadFileAsync_UrlOk()
         {
             await _scraper.DownloadFileAsync(@"http://www.google.com/images/branding/googlelogo/1x/googlelogo_color_150x54dp.png");
             Assert.IsTrue(File.Exists(@"www.google.com/images/branding/googlelogo/1x/googlelogo_color_150x54dp.png"));
+        }
+
+        /// <summary>
+        /// Tries to download a file and verify if the file was downloaded.
+        /// </summary>
+        [TestMethod]
+        public async Task DownLoadFileAsync_UrlMissingHttp()
+        {
+            try
+            {
+                await _scraper.DownloadFileAsync(@"google.com/images/branding/googlelogo/1x/googlelogo_color_150x54dp.png");
+                Assert.Fail("DownLoadFileAsync_UrlBad did not throw any exception when expecting InvalidOperationException");
+            }
+            catch (InvalidOperationException) { }
         }
 
         /// <summary>
@@ -60,6 +74,16 @@ namespace TestCases
         public void VerifyDownLoadUrl_ClearInputFromUrl()
         {
             Assert.AreEqual(@"http://someadress.com/sub/img/file.png", _scraper.VerifyDownloadUrl(@"http://someadress.com/sub/img/file.png?parameter=somevalue"));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void GetContentUrls_HtmlOk()
+        {
+            var html = File.ReadAllText(Directory.GetCurrentDirectory() + "\\..\\..\\Resources\\index.html");
+            Assert.IsTrue(_scraper.GetContentUrls(html).Count() == 18);
         }
     }
 }
